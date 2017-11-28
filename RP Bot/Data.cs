007b5d3ca1b585863e04629a9c45481a51628c6b
Log234 <ajsivesind@gmail.com>
2017-11-28 01:48:29 +0100

    Added data and such

---
+++ RP Bot/Data.cs
@@ -0,0 +1,35 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RP_Bot
+{
+    static class Data
+    {
+       static Dictionary<SocketChannel, Channel> channels = new Dictionary<SocketChannel, Channel>();
+    }
+
+    class Channel
+    {
+        DateTime joined;
+        DateTime idle;
+        Dictionary<string, Event> events = new Dictionary<string, Event>();
+    }
+
+    class Event
+    {
+        DateTime created;
+        DateTime idle;
+        SocketUser dm;
+        HashSet<SocketUser> admins = new HashSet<SocketUser>();
+        HashSet<Character> characters = new HashSet<Character>();
+        
+    }
+
+    class Character
+    {
+        string name;
+        int health;
+    }
+}
---
+++ RP Bot/Module.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RP_Bot
+{
+    using Discord.Commands;
+    using Discord.WebSocket;
+    using System.Threading.Tasks;
+
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        
+    }
+
+    [Group("event")]
+    public class EventModule : ModuleBase<SocketCommandContext>
+    {
+
+        // Start an event
+        [Command("start")]
+        [Summary("Starts a new RP event.")]
+        public async Task CreateEvent()
+        {
+            SocketUser dm = Context.Message.Author;
+            // TO-DO Implement logic
+
+            string eventName = "Test";
+            await ReplyAsync($"New event \"{eventName}\" started!\nType \"!event join {eventName}\" to join.");
+        }
+    }
+}
